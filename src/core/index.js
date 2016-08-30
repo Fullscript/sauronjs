2a1c8991447d045dda77917a9212caa7c32c1a6b
Alex Pilon <apilo088@gmail.com>
2016-08-30 19:07:32 +0000

    adjust exports

---
+++ src/core/index.js
@@ -0,0 +1,5 @@
+exports.instance = require('./sauron');
+exports.broadcast = require('./broadcast');
+exports.cache = require('./cache');
+exports.Service = require('./service');
+exports.Component = require('./component');
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
-/* BEGIN LEGACY EXPORTS */
-exports.insert = require('./util/insert');
-exports.instance = require('./core/sauron');
-exports.Component = require('./core/component');
+module.exports = require('./core');
+module.exports.util = require('./util');
 
-/* BEGIN CUSTOM EVENTS AVOID USING */
 /* TODO: with the broadcast refactor, this event system should be removed */
-function dispatchEvent(eventName) {
-  var event = document.createEvent('Event');
-  event.initEvent(eventName, true, true);
-  document.body.dispatchEvent(event);
-}
-
-exports.events = {
+module.exports.events = {
   dom: {
     update: function() {
-      dispatchEvent('sauron:dom:update');
+      var event = document.createEvent('Event');
+      event.initEvent('sauron:dom:update', true, true);
+      document.body.dispatchEvent(event);
     }
   }
 };
-/* END CUSTOM EVENTS */
-/* END LEGACY EXPORTS */
-
-// EXPORTS
-exports.util = require('./util');
